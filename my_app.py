d55ddf8c35c94a248ed211798b369c9b1abcf904
José Luis Méndez <mendezcarrillojl@gmail.com>
2023-04-05 18:03:45 -0600

    New template with tabs

--- my_app.py
+++ my_app.py
@@ -4,6 +4,8 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+from dash import Input, Output, html
+from tab_content import sales_tab, engagement_tab, tab_3
 
 app = Dash(
     __name__,
@@ -20,91 +22,35 @@ app.layout = html.Div(
             dark=True,
         ),
 
-        html.Div(
+        dbc.Card(
             [
-                # Indicadores
-                dbc.Row(
-                    [
-                        html.Div(
-                            children=[html.H1(["Indicadores"])]
-                        ),
-                        dbc.Col(
-                            html.Div(
-                                dbc.Card(
-                                    dbc.CardBody("This is some text within a card body")
-                                )
-                            ),
-                            width=3
-                        ),
-                        dbc.Col(
-                            html.Div(
-                                dbc.Card(
-                                    dbc.CardBody("This is some text within a card body")
-                                )
-                            ),
-                            width=3
-                        ),
-                        dbc.Col(
-                            html.Div(
-                                dbc.Card(
-                                    dbc.CardBody("This is some text within a card body")
-                                )
-                            ),
-                            width=3
-                        ),
-                        dbc.Col(
-                            html.Div(
-                                dbc.Card(
-                                    dbc.CardBody("This is some text within a card body")
-                                )
-                            ),
-                            width=3
-                        )
-                    ]
+                dbc.CardHeader(
+                    dbc.Tabs(
+                        [
+                            dbc.Tab(label = 'Sales', tab_id = 'tab-1'),
+                            dbc.Tab(label = 'Engagement', tab_id = 'tab-2'),
+                            dbc.Tab(label = 'Tab 3', tab_id = 'tab-3')
+                        ],
+                        id = 'card-tabs',
+                        active_tab = 'tab-1'
+                    )
                 ),
-                html.Br(),
-                # Más indicadores
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            html.Div(
-                                dbc.Card(
-                                    dbc.CardBody("This is some text within a card body")
-                                )
-                            ),
-                            width=3
-                        ),
-                        dbc.Col(
-                            html.Div(
-                                dbc.Card(
-                                    dbc.CardBody("This is some text within a card body")
-                                )
-                            ),
-                            width=3
-                        ),
-                        dbc.Col(
-                            html.Div(
-                                dbc.Card(
-                                    dbc.CardBody("This is some text within a card body")
-                                )
-                            ),
-                            width=3
-                        ),
-                        dbc.Col(
-                            html.Div(
-                                dbc.Card(
-                                    dbc.CardBody("This is some text within a card body")
-                                )
-                            ),
-                            width=3
-                        )
-                    ]
-                )
-            ],
-            className="px-4 pt-4"
-        )
+                dbc.CardBody(id = 'card-content')
+            ]
+        )  
     ]
 )
+
+@app.callback(
+    Output("card-content", "children"), [Input("card-tabs", "active_tab")]
+)
+def tab_content(active_tab):
+    if active_tab == 'tab-1':
+        return sales_tab
+    elif active_tab == 'tab-2':
+        return engagement_tab
+    elif active_tab == 'tab-3':
+        return tab_3
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ tab_content.py
@@ -0,0 +1,237 @@
+from dash import Dash, dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+from dash import Input, Output, html
+
+sales_tab = html.Div(
+            [
+                # Indicadores
+                dbc.Row(
+                    [
+                        html.Div(
+                            children=[html.H1(["Indicadores 1"])]
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        )
+                    ]
+                ),
+                
+                html.Br(),
+                
+                # Más indicadores
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        )
+                    ]
+                ),
+
+                html.Br(),
+
+                # Serie de tiempo
+                dbc.DropdownMenu(
+                    label = 'Selecciona un KPI',
+                    children = [
+                        dbc.DropdownMenuItem("Conversion Rate"),
+                        dbc.DropdownMenuItem("Sessions"),
+                        dbc.DropdownMenuItem("Clicks"),
+                    ]
+                ),
+                
+                html.Br(),
+
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            )
+                        )
+                    ]
+                ),
+
+                html.Br(),
+
+                # Adicionales
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ), width = 6
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ), width = 6
+                        )
+                    ]
+                ),
+            ],
+            className="px-4 pt-4"
+    )
+
+engagement_tab = html.Div(
+            [
+                # Indicadores
+                dbc.Row(
+                    [
+                        html.Div(
+                            children=[html.H1(["Indicadores 2"])]
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        )
+                    ]
+                ),
+                html.Br(),
+                # Más indicadores
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        )
+                    ]
+                )
+            ],
+            className="px-4 pt-4"
+        )
+
+tab_3 = html.Div(
+            [
+                # Indicadores
+                dbc.Row(
+                    [
+                        html.Div(
+                            children=[html.H1(["Indicadores 3"])]
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        )
+                    ]
+                ),
+                html.Br(),
+                # Más indicadores
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            html.Div(
+                                dbc.Card(
+                                    dbc.CardBody("This is some text within a card body")
+                                )
+                            ),
+                            width=3
+                        )
+                    ]
+                )
+            ],
+            className="px-4 pt-4"
+    )
